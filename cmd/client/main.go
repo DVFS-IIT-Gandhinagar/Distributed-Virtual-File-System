42b90ccc1c71c6773a829bd733e83d92eb5200e9
Umang Shikarvar <umang.shikarvar@iitgn.ac.in>
2026-02-04 17:30:28 +0530

    flag for username and ip

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,36 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/umangshikarvar/dvfs/internal/client"
 )
 
 func main() {
 	// Client configuration
-	username := "romit"
-	serverAddress := "127.0.0.1:50051"
-	// Parse command line args
-	if len(os.Args) > 1 {
-		username = os.Args[1]
-	}
-	 if len(os.Args) > 2 {
-		serverAddress = os.Args[2] + ":50051"
-	}
+	username := flag.String("username", "romit", "enter username")
+	ip_addr := flag.String("ip_addr", "127.0.0.1", "enter ip_addr")
+	flag.Parse()
+
+	serverAddress := *ip_addr + ":50051"
 
 	// Create and connect client
-	c := client.NewClient(username)
-	
-	fmt.Printf("Connecting to server at %s as user %s...\n", serverAddress, username)
-	
+	c := client.NewClient(*username)
+
+	fmt.Printf("Connecting to server at %s as user %s...\n", serverAddress, *username)
+
 	if err := c.Connect(serverAddress); err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
-	
+
 	fmt.Printf("Connected successfully!\n\n")
-	
+
 	// Start interactive command handler
 	handler := client.NewCommandHandler(c)
 	handler.Start()
