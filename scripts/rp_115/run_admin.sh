1ce128f11505a4db3cddca025e6a2b5ed92bb22c
jsmaskeen <jsmaskeen@gmail.com>
2026-09-05 02:08:07 +0530

    bug fix-indexing

--- scripts/rp_115/run_admin.sh
+++ scripts/rp_115/run_admin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tmux new-session -d -s admin 'cd Distributed-Virtual-File-System/bin/; ./admin -port=8080 -state_file=./metaserver_state.json -ssh_user=$(whoami) -ssh_key=~/.ssh/id_ed25519 -repo_path=~/Distributed-Virtual-File-System'
+tmux new-session -d -s admin 'cd Distributed-Virtual-File-System/; ./bin/admin -port=8080 -state_file=./bin/metaserver_state.json -ssh_user=$(whoami) -ssh_key=~/.ssh/id_ed25519 -repo_path=~/Distributed-Virtual-File-System'
 
